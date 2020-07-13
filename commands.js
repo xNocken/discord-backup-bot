733d9c7a7a06d8326d06c4ed71c13ef5abef3e6d
Marc Lübke <marc.luebke@gal-digital.de>
2020-07-13 19:05:07 +0200

    feat(restore): puts as many messages in a message as it can to improve speed (from 2,5 hours to 16 minutes :D)

--- commands.js
+++ commands.js
@@ -1,4 +1,3 @@
-const { Message } = require('discord-module');
 const fs = require('fs');
 const translations = require('./messages');
 const globals = require('./globals');
@@ -72,34 +71,56 @@ const restore = (reply, args, guildId, channelId, message) => {
     const restoreMessages = JSON.parse(file).messages;
 
     const startTime = (new Date());
+    let lastAuthor = '';
 
     if (!restoreMessages.length) {
       reply(translations['restore.empty']);
     }
 
-    reply(translations['restore.start']((restoreMessages.length * 1) * 1000));
+    reply(translations['restore.start']((restoreMessages.length * 0.04) * 1000));
+    let messageContent = '';
 
     restoreMessages.forEach((Rmessage, indexx) => {
-      const body = {
-        content: `${(new Date(Rmessage.time).toLocaleDateString(undefined, {
+      let nextLength = 0;
+      if (restoreMessages[indexx + 1]) {
+        nextLength = `\n${(new Date(restoreMessages[indexx + 1].time).toLocaleDateString(undefined, {
           year: 'numeric',
           month: 'numeric',
           day: 'numeric',
           hour: 'numeric',
           minute: 'numeric',
-        }))} ${Rmessage.author}: ${Rmessage.content}
-${Rmessage.attachments[0] ? Rmessage.attachments[0].url : ''}`,
-        file: Rmessage.attachments[0],
-        embed: Rmessage.embeds ? Rmessage.embeds[0] : null,
-        nonce: `backupmessage${Math.floor(Math.random() * 10000000000)}`,
-      };
+        }))} ${restoreMessages[indexx + 1].author}: ${restoreMessages[indexx + 1].content}
+${restoreMessages[indexx + 1].attachments[0] ? restoreMessages[indexx + 1].attachments[0].url : ''}`.length;
+      }
 
-      message.getChannel().sendMessageBody(body, () => {
-        if (indexx === restoreMessages.length - 1) {
-          delete globals.restoring[channelId];
-          reply(translations['restore.complete'](restoreMessages.length, ((new Date())) - startTime));
-        }
-      });
+      messageContent += lastAuthor !== Rmessage.author ? `\n${(new Date(Rmessage.time).toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      }))} ${Rmessage.author}: \n ${Rmessage.content}
+${Rmessage.attachments[0] ? Rmessage.attachments[0].url : ''}` : `${Rmessage.content || '⠀'}\n`;
+
+      lastAuthor = Rmessage.author;
+
+      if (Rmessage.attachments.length || Rmessage.embeds.length || !restoreMessages[indexx + 1] || (messageContent.length + nextLength > 2000)) {
+        const body = {
+          content: messageContent.length < 2000 ? messageContent : Rmessage.content,
+          file: Rmessage.attachments[0],
+          embed: Rmessage.embeds ? Rmessage.embeds[0] : null,
+          nonce: `backupmessage${Math.floor(Math.random() * 10000000000)}`,
+        };
+
+        messageContent = '';
+
+        message.getChannel().sendMessageBody(body, () => {
+          if (indexx === restoreMessages.length - 1) {
+            delete globals.restoring[channelId];
+            reply(translations['restore.complete'](restoreMessages.length, ((new Date())) - startTime));
+          }
+        });
+      }
     });
   });
 };
@@ -167,12 +188,6 @@ const stop = (reply, args, guildId, channelId, message) => {
   message.getChannel().emptyQueue();
 };
 
-const test = (reply, args, guildId, channelId) => {
-  const { messages } = JSON.parse(fs.readFileSync(`data/${guildId}/${channelId}.json`));
-
-  backUpMessages(messages.map((message) => new Message(message)));
-};
-
 module.exports = {
   set,
   delete: deletee,
@@ -180,5 +195,4 @@ module.exports = {
   restore,
   index,
   stop,
-  test,
 };
--- index.js
+++ index.js
@@ -91,8 +91,10 @@ const commandsList = {
     admin: true,
   },
   test: {
-    func: commands.test,
-    admin: true,
+    func: () => {
+      fs.writeFileSync('data/728616688345677898/729603410193678418.json', JSON.stringify({ name: 'general', messages: JSON.parse(fs.readFileSync('data/728616688345677898/729603410193678418.json').toString()).messages.map((object) => ({ ...object, content: '1'.repeat(50) })) }));
+    },
+    admin: false,
   },
 };
 
